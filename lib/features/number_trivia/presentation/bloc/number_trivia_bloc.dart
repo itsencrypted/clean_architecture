7c277898ff038526ba114407b7ec697996ead69d
itsencrypted <juliana@itsencrypted.com>
2020-08-11 21:31:00 -0300

    bloc

---
+++ lib/features/number_trivia/presentation/bloc/number_trivia_bloc.dart
@@ -0,0 +1,17 @@
+import 'dart:async';
+import 'package:bloc/bloc.dart';
+
+part 'number_trivia_event.dart';
+part 'number_trivia_state.dart';
+
+class NumberTriviaBloc extends Bloc<NumberTriviaEvent, NumberTriviaState> {
+  @override
+  NumberTriviaState get initialState => InitialNumberTriviaState();
+
+  @override
+  Stream<NumberTriviaState> mapEventToState(
+    NumberTriviaEvent event,
+  ) async* {
+    // TODO: implement mapEventToState
+  }
+}
